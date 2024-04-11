cd6786c5ede04d36753e7aa2a47684d67c130559
vladimir.kuzomenskyi <kuzomenskyi@gmail.com>
2024-04-11 11:18:05 -0400

    Initial commit

---
+++ MultiSliderIsNotDisplayedCorrectly/MultiSliderIsNotDisplayedCorrectlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiSliderIsNotDisplayedCorrectlyApp.swift
+//  MultiSliderIsNotDisplayedCorrectly
+//
+//  Created by vladimir.kuzomenskyi on 11.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct MultiSliderIsNotDisplayedCorrectlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
